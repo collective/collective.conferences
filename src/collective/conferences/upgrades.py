9d32b14120929369eae510971143de42e92adc6f
andreasma <maand@gmx.de>
2020-05-09 17:12:57 +0200

    add upgrades zcml and module

---
+++ src/collective/conferences/upgrades.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from plone.app.upgrade.utils import loadMigrationProfile
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def reload_gs_profile(context):
+    loadMigrationProfile(
+        context,
+        'profile-collective.conferences:default',
+    )
+

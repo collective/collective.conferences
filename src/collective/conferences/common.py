71cf912da597eb8e0fc4d1f5912fbb3b94f00f60
andreasma <maand@gmx.de>
2020-05-13 17:51:22 +0200

    Add a module for common functions etc.

---
+++ src/collective/conferences/common.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from collective.conferences import _
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+
+
+yesnochoice = SimpleVocabulary(
+    [SimpleTerm(value=0, title=_(u'No')),
+     SimpleTerm(value=1, title=_(u'Yes'))],
+)

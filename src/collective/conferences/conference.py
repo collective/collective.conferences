6844d43131f2b07b9795b19f7904d7be590d247f
andreasma <maand@gmx.de>
2020-05-19 20:46:36 +0200

    Add fields to set choices for length of talks, workshops and conferencebreaks.

--- src/collective/conferences/conference.py
+++ src/collective/conferences/conference.py
@@ -23,3 +23,28 @@ class IConference(model.Schema):
         description=_(u"Write information about the conference"),
         required=False,
     )
+
+
+    break_length = schema.List(
+        title=_(u"Length Of Conference Breaks"),
+        description=_(u"Fill in the time slots for conference breaks in minutes. Use a new line for every "
+                      u"value / talk length. Write only the numbers without the addition 'minutes'."),
+        value_type=schema.TextLine(),
+        required=True,
+    )
+
+    talk_length = schema.List(
+        title=_(u"Length Of Talks"),
+        description=_(u"Fill in the time slots for conference talks in minutes. Use a new line for every "
+                      u"value / talk length. Write only the numbers without the addition 'minutes'."),
+        value_type=schema.TextLine(),
+        required=True,
+    )
+
+    workshop_length = schema.List(
+        title=_(u"Length Of Workshops"),
+        description=_(u"Fill in the time slots for workshops at the conference in minutes. Use a new line for every "
+                      u"value / workshop length. Write only the numbers without the addition 'minutes'."),
+        value_type=schema.TextLine(),
+        required=True,
+    )

3c95b9ab3777232978718fe05bc9f836ff894599
andreasma <maand@gmx.de>
2020-06-09 20:04:40 +0200

    Create a new vocabulary for conference tracks and add it as a named utility to configure.zcml.

--- src/collective/conferences/vocabularies.py
+++ src/collective/conferences/vocabularies.py
@@ -57,3 +57,22 @@ class ConferenceRoomVocabulary(object):
 
 
 ConferenceRoomVocabularyFactory = ConferenceRoomVocabulary()
+
+
+
+@implementer(IVocabularyFactory)
+class ConferenceTrackVocabulary(object):
+
+    def __call__(self, context=None):
+        terms = []
+        # Use getObject since the DataConverter expects a real object.
+        for brain in api.content.find(portal_type='collective.conferences.track', sort_on='sortable_title'):
+            terms.append(SimpleTerm(
+                value=brain.getObject(),
+                token=brain.UID,
+                title=u'{} ({})'.format(brain.Title, brain.getPath()),
+            ))
+        return SimpleVocabulary(terms)
+
+
+ConferenceTrackVocabularyFactory = ConferenceTrackVocabulary()

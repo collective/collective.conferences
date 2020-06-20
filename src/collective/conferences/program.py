83c5743748064dfac074fd8f84bba0617b513cbb
andreasma <maand@gmx.de>
2020-06-20 18:48:05 +0200

    Remove old indexers from program module.

--- src/collective/conferences/program.py
+++ src/collective/conferences/program.py
@@ -91,21 +91,6 @@ class IProgram(model.Schema):
 # Indexers
 
 
-@indexer(IProgram)
-def startIndexer(obj):
-    if obj.start is None:
-        return None
-    return DateTime(obj.start.isoformat())
-# grok.global_adapter(startIndexer, name="start")
-
-
-@indexer(IProgram)
-def endIndexer(obj):
-    if obj.end is None:
-        return None
-    return DateTime(obj.end.isoformat())
-# grok.global_adapter(endIndexer, name="end")
-
 
 @indexer(ITrack)
 def tracksIndexer(obj):

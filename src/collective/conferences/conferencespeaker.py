f464ff93ddb372b12c15d2b68b27110a557cfb0e
andreasma <maand@gmx.de>
2020-06-26 08:59:37 +0200

    Pep8 and isort fixes.

--- src/collective/conferences/conferencespeaker.py
+++ src/collective/conferences/conferencespeaker.py
@@ -10,13 +10,14 @@ from zope.interface import Invalid
 
 import re
 
+
 checkEmail = re.compile(
     r"[a-zA-Z0-9._%-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,4}").match
 
 
 def validateEmail(value):
     if not checkEmail(value):
-        raise Invalid(_(u"Invalid email address"))
+        raise Invalid(_(u'Invalid email address'))
     return True
 
 
@@ -25,74 +26,75 @@ class IConferenceSpeaker(model.Schema):
     """
 
     lastname = schema.TextLine(
-        title=_(u"Last name"),
+        title=_(u'Last name'),
     )
 
     firstname = schema.TextLine(
-        title=_(u"First name"),
+        title=_(u'First name'),
         required=True,
     )
 
     street = schema.TextLine(
-        title=_(u"Street"),
-        description=_(u"For those requiring visa, please add your full postal address details"),
+        title=_(u'Street'),
+        description=_(u'For those requiring visa, please add your full postal address details'),
         required=False,
     )
 
     city = schema.TextLine(
-        title=_(u"City"),
-        description=_(u"For those requiring visa, please add your full postal address details"),
+        title=_(u'City'),
+        description=_(u'For those requiring visa, please add your full postal address details'),
         required=False,
     )
 
     postalcode = schema.TextLine(
-        title=_(u"Postal Code"),
-        description=_(u"For those requiring visa, please add your full postal address details"),
+        title=_(u'Postal Code'),
+        description=_(u'For those requiring visa, please add your full postal address details'),
         required=False,
     )
 
     country = schema.TextLine(
-        title=_(u"Country"),
-        description=_(u"For those requiring visa, please add your full postal address details"),
+        title=_(u'Country'),
+        description=_(u'For those requiring visa, please add your full postal address details'),
         required=False,
     )
 
     email = schema.ASCIILine(
-        title=_(u"Your email address"),
+        title=_(u'Your email address'),
         constraint=validateEmail,
         required=True,
     )
 
     telephonenumber = schema.TextLine(
-        title=_(u"Telephone Number"),
+        title=_(u'Telephone Number'),
         description=_(
-            u"Please fill in your telephone number so that we could get in contact with you by phone if necessary."),
+            u'Please fill in your telephone number so that we could get in contact with you by phone if necessary.'),
         required=False,
     )
     mobiletelepone = schema.TextLine(
-        title=_(u"Mobile Telephone Number"),
+        title=_(u'Mobile Telephone Number'),
         description=_(
-            u"Please fill in your mobile telephone number so that we could get in contact with you during the conference."),
+            u'Please fill in your mobile telephone number so that we could get in contact with you '
+            u'during the conference.'),
         required=True,
     )
 
     organisation = schema.TextLine(
-        title=_(u"Organisation"),
+        title=_(u'Organisation'),
         required=False,
     )
 
     description = schema.Text(
-        title=_(u"A short bio"),
+        title=_(u'A short bio'),
     )
 
     bio = RichText(
-        title=_(u"Bio"),
+        title=_(u'Bio'),
         required=False
     )
 
     picture = NamedBlobImage(
-        title=_(u"Picture"),
-        description=_(u"Please upload an image"),
+        title=_(u'Picture'),
+        description=_(u'Please upload an image'),
         required=False,
     )
 
@@ -106,9 +108,9 @@ def notifyUser(self, event):
     if not sender:
         return
 
-    subject = "Is this you?"
-    message = "A speaker /leader of a workshop called %s was added here %s. If this is you, everything is fine." % (
-    self.title, self.absolute_url(),)
+    subject = 'Is this you?'
+    message = 'A speaker /leader of a workshop called %s was added here %s. If this is you, everything is fine.' % (
+        self.title, self.absolute_url(),)
 
     api.portal.send_email(
         recipient='{0}'.format(email),
@@ -140,8 +142,8 @@ class ConferenceSpeakerView(BrowserView):
         catalog = api.portal.get_tool(name='portal_catalog')
 
         # execute a search
-        results = catalog(portal_type="collective.conferences.talk",
-                          review_state="published")
+        results = catalog(portal_type='collective.conferences.talk',
+                          review_state='published')
         # examine the results
         for brain in results:
             start = brain.start
@@ -154,8 +156,8 @@ class ConferenceSpeakerView(BrowserView):
     def speakertalks(self):
 
         speakername = self.context.title
-        results = api.content.find(portal_type="collective.conferences.talk",
-                                   review_state="published",
+        results = api.content.find(portal_type='collective.conferences.talk',
+                                   review_state='published',
                                    presenters=speakername)
 
         objects = []
@@ -168,8 +170,8 @@ class ConferenceSpeakerView(BrowserView):
     def speakerworkshops(self):
 
         speakername = self.context.title
-        results = api.content.find(portal_type="collective.conferences.workshop",
-                                   review_state="published",
+        results = api.content.find(portal_type='collective.conferences.workshop',
+                                   review_state='published',
                                    workshopleader=speakername)
 
         objects = []

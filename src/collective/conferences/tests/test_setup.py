0a1aa5bd46c3dfb5dbf681622c0ca640d3835f14
andreasma <maand@gmx.de>
2020-06-27 12:27:00 +0200

    Fix import.

--- src/collective/conferences/tests/test_setup.py
+++ src/collective/conferences/tests/test_setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
-from collective.addons.testing import COLLECTIVE_CONFERENCES_INTEGRATION_TESTING  # noqa: E501
+from collective.conferences.testing import COLLECTIVE_CONFERENCES_INTEGRATION_TESTING  # noqa: E501
 from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID

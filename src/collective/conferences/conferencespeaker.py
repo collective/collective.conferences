93f02c5ce8da6d9a464d409f5fc0a3d784a42045
andreasma <maand@gmx.de>
2020-07-05 15:40:15 +0200

    Pep8 fix and remove old tests.

--- src/collective/conferences/conferencespeaker.py
+++ src/collective/conferences/conferencespeaker.py
@@ -10,7 +10,6 @@ from zope.interface import Invalid
 
 import re
 
-
 checkEmail = re.compile(
     r'[a-zA-Z0-9._%-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,4}').match
 
--- src/collective/conferences/tests/test_speaker.py
+++ src/collective/conferences/tests/test_speaker.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
-from collective.conferences.conferencespeaker import IConferenceSpeaker
 from collective.conferences.testing import COLLECTIVE_CONFERENCES_INTEGRATION_TESTING
 from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
-from plone.dexterity.interfaces import IDexterityFTI
-from zope.component import createObject
-from zope.component import queryUtility
+
 
 import unittest
 
@@ -21,29 +18,6 @@ class TestPresenterIntegration(unittest.TestCase):
         api.content.create(type='collective.conferences.speakerfolder', title='test-folder', container=portal)
         setRoles(self.portal, TEST_USER_ID, ['Member'])
         self.folder = self.portal['test-folder']
-
-    def test_adding(self):
-        portal = api.portal.get()
-        testfolder = portal['test-folder']
-        api.content.create(container=testfolder, type='collective.conferences.conferencespeaker',
-                           title='conferencespeaker1')
-        p1 = self.folder['test-folder/conferencespeaker1']
-        self.assertTrue(IConferenceSpeaker.providedBy(p1))
-
-    def test_fti(self):
-        fti = queryUtility(IDexterityFTI, name='collective.conferences.conferencespeaker')
-        self.assertNotEqual(None, fti)
-
-    def test_schema(self):
-        fti = queryUtility(IDexterityFTI, name='collective.conferences.conferencespeaker')
-        schema = fti.lookupSchema()
-        self.assertEqual(IConferenceSpeaker, schema)
-
-    def test_factory(self):
-        fti = queryUtility(IDexterityFTI, name='collective.conferences.conferencespeaker')
-        factory = fti.factory
-        new_object = createObject(factory)
-        self.assertTrue(IConferenceSpeaker.providedBy(new_object))
 
 
 def test_suite():

484925dd733fa5b9ae751210a71f75b04147fcef
andreasma <maand@gmx.de>
2020-07-28 13:45:34 +0200

    Use safe_unicode for the unicode strings.

--- src/collective/conferences/talksfolder.py
+++ src/collective/conferences/talksfolder.py
@@ -2,24 +2,23 @@
 from collective.conferences import _
 from plone.autoform import directives
 from plone.supermodel import model
+from Products.CMFPlone.utils import safe_unicode
 from Products.Five import BrowserView
 from zope import schema
 
 
 class ITalksfolder(model.Schema):
-
     directives.mode(title='hidden')
     title = schema.TextLine(
-        title=_(u'Name Of The Folder For Talks'),
-        default=_(u'Talks'),
+        title=_(safe_unicode('Name Of The Folder For Talks')),
+        default=_(safe_unicode('Talks')),
     )
 
     description = schema.Text(
-        title=_(u'Talk Folder Description'),
+        title=_(safe_unicode('Talk Folder Description')),
         required=False,
     )
 
 
 class TalksfolderView(BrowserView):
-
     pass

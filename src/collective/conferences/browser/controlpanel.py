a980b3aa37c568e8942bef8efcfb0b1a6376d0c7
andreasma <maand@gmx.de>
2020-08-01 13:21:02 +0200

    Add an entry for allowed conference video files.

--- src/collective/conferences/browser/controlpanel.py
+++ src/collective/conferences/browser/controlpanel.py
@@ -76,6 +76,13 @@ class ICollectiveconferenceControlPanel(Interface):
         default=safe_unicode('otp|pdf|zip'),
     )
 
+    allowed_video_file_extensions = schema.TextLine(
+        title=_(safe_unicode('Allowed file exensions for conference videos.')),
+        description=_(safe_unicode('Fill in the allowed file extensions for the videos of '
+                                   "conference talks / workshops, separated by a pipe '|'.")),
+        default=safe_unicode('mp4|mpg'),
+    )
+
 
 class CollectiveconferenceControlPanelForm(RegistryEditForm):
     schema = ICollectiveconferenceControlPanel

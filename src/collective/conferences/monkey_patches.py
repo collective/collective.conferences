7b8d26e0857066cbee927f3bec48e517838517c0
andreasma <maand@gmx.de>
2020-08-05 20:25:00 +0200

    Add a monkey patch to solve the folder view of talks and workshops with a RelationList/SelectFieldWidget ("ValueError: Circular reference detected")

---
+++ src/collective/conferences/monkey_patches.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from z3c.relationfield.schema import RelationList
+from z3c.relationfield.relation import RelationValue
+from DateTime import DateTime
+import datetime
+import Missing
+
+
+def conf_utils_custom_json_handler(obj):
+    if obj == Missing.Value:
+        return None
+    if type(obj) in (datetime.datetime, datetime.date):
+        return obj.isoformat()
+    if type(obj) == DateTime:
+        dt = DateTime(obj)
+        return dt.ISO()
+    if type(obj) == set:
+        return list(obj)
+    if type(obj) == RelationValue:
+        return obj.to_path.split('/')[-1]
+    if type(obj) == RelationList:
+        return list(item.to_path.split('/')[-1] for item in obj)
+    return obj

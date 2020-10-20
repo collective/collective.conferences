92d9cbc08abac6f65974cf4402470556c7adf56c
andreasma <maand@gmx.de>
2020-10-20 14:43:08 +0200

    Update to version 0.6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Andreas Mantke'
 author = 'Andreas Mantke'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3'
+release = '0.6'
 
 
 # -- General configuration ---------------------------------------------------

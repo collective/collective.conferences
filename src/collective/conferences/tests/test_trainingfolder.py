3f9e85dddd6dc3fd7fb603ca7ca92f55796bdc67
andreasma <maand@gmx.de>
2020-11-21 21:32:15 +0100

    isort fix.

--- src/collective/conferences/tests/test_trainingfolder.py
+++ src/collective/conferences/tests/test_trainingfolder.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from collective.conferences.trainingfolder import ITrainingFolder
 from collective.conferences.testing import COLLECTIVE_CONFERENCES_INTEGRATION_TESTING
+from collective.conferences.trainingfolder import ITrainingFolder
 from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
